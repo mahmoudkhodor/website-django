035096628cf9e42ad5021cc14da339ba8ed248cd
Mahmoud Khodor <mahmoud.khodor@hotmail.com>
2019-11-14 13:20:58 +0200

    feat: added admin page

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Tutorial
 # Register your models here.
+
+
+class TutorialAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+        ("content", {"fields": ["tutorial_content"]})
+    ]
+
+
+admin.site.register(Tutorial)
