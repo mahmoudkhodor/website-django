1aa4bf081d4b47cd7f6e3c5b372b4aa1d42d0c65
Mahmoud Khodor <mahmoud.khodor@DESKTOP-5VCEVDO>
2019-11-15 14:46:02 +0200

    feat: added messages and includes

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 # Create your views here.
 
 
@@ -17,11 +18,13 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {}")
             login(request, user)
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}:form.error_messages[msg]")
 
     form = UserCreationForm
     return render(request, "main/register.html", context={"from": form})
