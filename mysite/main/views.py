7acbd1fa8cd4453e8c7979da1e148cdc8a09d113
Mahmoud Khodor <mahmoud.khodor@DESKTOP-5VCEVDO>
2019-11-15 16:24:14 +0200

    feat: adding a modified forms

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 # Create your views here.
@@ -28,3 +28,28 @@ def register(request):
 
     form = UserCreationForm
     return render(request, "main/register.html", context={"from": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
+
+
+def login_request(request):
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data('username')
+            password = form.cleaned_data('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                retrun redirect("main:homepage")
+            else:
+                message.error(request, "Invalid username or password")
+
+    form = AuthenticationForm()
+    return render(request, "main/login.html", {"form": form})
