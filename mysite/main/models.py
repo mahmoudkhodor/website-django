474aee1485219eec2749c6c3a35508885ae0072f
Mahmoud Khodor <mahmoud.khodor@DESKTOP-5VCEVDO>
2019-11-15 17:01:20 +0200

    feat: added dynamic sidebar

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -4,6 +4,12 @@ from datetime import datetime
 # Create your models here.
 
 
+class TutorialCategory(models.Model):
+    tutorial_category = models.CharField(max_length=200)
+    category_summary = models.CharField(max_length=200)
+    category_slug = models.CharField(max_length=200)
+
+
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
